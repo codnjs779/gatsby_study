f287e77701451da31f32040288e62c68b4daf317
최채원 <codnjs779@naver.com>
2023-01-10 22:00:16 +0900

    gatsby setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
+
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
